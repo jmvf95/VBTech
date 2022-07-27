62a8220a3f0d24f835908eea7a74807470a92453
jmvf95 <jmvallejofdez@gmail.com>
2022-07-27 20:17:30 +0200

    Creacion del componente contacto y modificacion de la cabecera 27-07.2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavegacionComponent
+    NavegacionComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
